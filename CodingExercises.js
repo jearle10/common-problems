4faa506da6d710ad6122d1067168dbef44044b8c
Jian Earle <jianearle93@googlemail.com>
2020-01-02 20:37:40 +0000

    Solution for list of fibonacci numbers added

--- CodingExercises.js
+++ CodingExercises.js
@@ -1,10 +1,26 @@
-//  nth fibonacci
+// Title: nth fibonacci
+// Description: Find the nth fibonacci number and return it
 
 function fibN(n) {
   fibArray = [0, 1];
   if (n <= 2) {
     return fibArray[n - 1];
   } else {
-    return fibN(n - 2) + fibN(n - 1);
+    console.log(fibN(n - 1) + fibN(n - 2));
   }
+}
+
+// Title: List of fibonacci numbers
+// Description: Return a list of n fibonacci numbers
+
+function fibList(n) {
+  fibArray = [0, 1];
+  if (n <= 2) {
+    return fibArray;
+  } else {
+    for (var i = 2; i < n; i++) {
+      fibArray.push(fibArray[i - 1] + fibArray[i - 2]);
+    }
+  }
+  console.log(fibArray);
 }

42377b665cb1d81fd7e779e8893ebb622a2e1f55
Jian Earle <jianearle93@googlemail.com>
2020-02-21 21:33:46 +0000

    typo in closestBST.js

--- closestValueInBST.js
+++ closestValueInBST.js
@@ -1,49 +1,46 @@
-// Find the closest value in a Binary search tree - Assume there will only be one closest value 
-
+// Find the closest value in a Binary search tree - Assume there will only be one closest value
 
 // Average O(log(n)) time | O(log(n)) space
 // Worst O(n) time | O(n) space
 
 function findClosestValueInBst(tree, target) {
-	return searchTree(tree, target , Infinity);
+  return searchTree(tree, target, Infinity);
 }
 
-function searchTree(tree, target, closestNode){
-	if(tree === null) {
-		return closestNode;
-	}
-	if(Math.abs(target - closestNode) > Math.abs(target - tree.value)){
-		closestNode = tree.value;
-	}
-	if(target < tree.value){
-		return searchTree(tree.left, target, closestNode);
-	} else if (target > tree.value){
-		return searchTree(tree.right , target , closestNode);
-	} else {
-		return closestNode;
-	}
+function searchTree(tree, target, closestNode) {
+  if (tree === null) {
+    return closestNode;
+  }
+  if (Math.abs(target - closestNode) > Math.abs(target - tree.value)) {
+    closestNode = tree.value;
+  }
+  if (target < tree.value) {
+    return searchTree(tree.left, target, closestNode);
+  } else if (target > tree.value) {
+    return searchTree(tree.right, target, closestNode);
+  } else {
+    return closestNode;
+  }
 }
 
- // Best O(log(n)) time | O(1) space 
- // Worst O(n) time | O(1) space
+// Best O(log(n)) time | O(1) space
+// Worst O(n) time | O(1) space
 
-function findClosestValueInBst(tree, target){
-    return searchTree(tree, target, Infinity)
+function findClosestValueInBst(tree, target) {
+  return searchTree(tree, target, Infinity);
 }
-function searchTree(tree, target , closestNode)
-        while (tree != null){
-            if(Math.abs(target - closestNode) > Math.abs(target - tree.value)){
-                closestNode = tree.value
-            }
-            if(target < tree.value){
-                tree = tree.left
-            } else if ( target > tree.value){
-                tree = tree.right
-            } else{
-                break
-            }
-        }
+function searchTree(tree, target, closestNode) {
+  while (tree != null) {
+    if (Math.abs(target - closestNode) > Math.abs(target - tree.value)) {
+      closestNode = tree.value;
     }
-    return closestNode
+    if (target < tree.value) {
+      tree = tree.left;
+    } else if (target > tree.value) {
+      tree = tree.right;
+    } else {
+      break;
+    }
+  }
 }
-
+return closestNode;

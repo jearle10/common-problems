8d3f3869471fc9fda7828219ba1486b3a59a9a96
jce1009 <jianearle93@googlemail.com>
2020-02-24 12:40:39 +0000

    Updated solution for closest BST

--- Python/closestValueBST.py
+++ Python/closestValueBST.py
@@ -16,3 +16,20 @@ def search(tree, target, closest):
         return search(tree.right, target, closest)
     else:
         return closest
+
+# O(log(n)) time | O(1) time
+
+def findClosestValueInBst(tree , target):
+    return search(tree, target, float("inf"))
+
+def search(tree, target, closest):
+    while tree is not None:
+		if abs(target - closest) > abs(target - tree.value):
+			closest = tree.value
+		if target < tree.value:
+			tree = tree.left
+		elif target > tree.value:
+			tree = tree.right
+		else:
+			break	
+	return closest
